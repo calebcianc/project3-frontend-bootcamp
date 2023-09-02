4175de53d8b91f1041c9f9ea149c2a8e5f37273c
calebcianc <calebcc.castro@gmail.com>
2023-09-02 11:23:29 +0800

    Changed code so the isPublic conditional to show maxPax and genderPreference works

--- src/Components/GenerateItineraryModal.js
+++ src/Components/GenerateItineraryModal.js
@@ -77,7 +77,7 @@ export default function GenerateItineraryModal({ modalView, handleClose }) {
     };
 
     try {
-      const response = await fetch("http://localhost:3001/generateItinerary", {
+      const response = await fetch("http://localhost:3000/generateItinerary", {
         method: "POST",
         headers: { "Content-type": "application/json" },
         body: JSON.stringify(itineraryInputs),
@@ -221,7 +221,7 @@ export default function GenerateItineraryModal({ modalView, handleClose }) {
           <br />
           <br />
 
-          {isPublic == "private" ? null : (
+          {isPublic === false ? null : (
             <div>
               <form>
                 <label for="max-pax">
