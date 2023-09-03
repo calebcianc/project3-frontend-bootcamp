b3a46614b2873fe8bc47e1dc8e8186f79272f27d
khloeli <chloelihuimin@gmail.com>
2023-09-03 15:23:59 +0800

    add card shadow

--- src/Components/ItineraryCard.js
+++ src/Components/ItineraryCard.js
@@ -29,7 +29,15 @@ export default function ItineraryCard({ itinerary }) {
   console.log("users number", users.length);
 
   return (
-    <Card sx={{ width: "calc(100% - 8mm)", m: "4mm", borderRadius: "4px" }}>
+    <Card
+      sx={{
+        width: "calc(100% - 8mm)",
+        m: "4mm",
+        borderRadius: "4px",
+        boxShadow:
+          "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+      }}
+    >
       <Box position="relative">
         <CardMedia
           component="img"
