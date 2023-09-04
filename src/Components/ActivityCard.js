810068bd594265ff752bce335924d242284d7b71
calebcianc <calebcc.castro@gmail.com>
2023-09-04 23:21:19 +0800

    created working activitycard component. now left to style

--- src/Components/ActivityCard.js
+++ src/Components/ActivityCard.js
@@ -1,8 +1,67 @@
-export default function ActivityCard() {
+import { useEffect, useState } from "react";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+  Box,
+} from "@mui/material";
+import axios from "axios";
+import { BACKEND_URL } from "../constants.js";
+import UserIcon from "./UserIcon.js";
+import { convertToDDMMYYYY } from "../utils/utils";
+
+export default function ActivityCard({ key, activity }) {
+  console.log("activity: ", JSON.stringify(activity));
+
+  const date = new Date(activity["date"]);
+  // Extract the various parts of the date
+  const day = date.getUTCDate();
+  const month = date.getUTCMonth(); // Month is 0-indexed
+  const year = date.getUTCFullYear().toString().substr(-2); // Get the last two digits of the year
+
+  // Month array to convert month number to month abbreviation
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  // Construct the formatted string
+  const formattedDate = `${day} ${monthNames[month]} ${year}`;
+
+  const timeOfDay = activity["timeOfDay"];
+  const capitalizedTimeOfDay =
+    timeOfDay.charAt(0).toUpperCase() + timeOfDay.slice(1).toLowerCase();
+
+  const today = new Date();
+  const isPastDate = activity["date"] < today;
+
   return (
-    <div>
-      Activity card showing key information e.g., Name, Description, Suggested
-      duration
-    </div>
+    <Card>
+      <CardContent>
+        <Typography
+          variant="body2"
+          color={isPastDate ? "text.secondary" : "text.primary"}
+        >
+          {formattedDate} ({capitalizedTimeOfDay})
+          <br />
+          {activity["name"]} @ {activity["location"]}
+          <br />
+          <em>{activity["description"]}</em>
+        </Typography>
+      </CardContent>
+    </Card>
   );
 }
--- src/Components/ActivityList.js
+++ src/Components/ActivityList.js
@@ -11,15 +11,43 @@ export default function ActivityList({
   };
 
   return (
-    <div style={{ overflow: "auto" }}>
+    <div
+      style={{
+        maxHeight: "calc(100vh - 64px - 56px )",
+        overflow: "auto",
+      }}
+    >
       <div style={{ display: "flex" }}>
         <Button onClick={handleGoBack}>Go back</Button>
       </div>
-      Activity List
-      <br />
-      GPT output: {JSON.stringify(itineraryActivities)}
-      <br />
-      <ActivityCard />
+      {itineraryActivities.map((element, index) => {
+        const activities = element.activities;
+        const filteredActivities = activities.filter(
+          (activity) => activity.itineraryId === selectedItinerary
+        );
+
+        const sortedActivities = filteredActivities.sort((a, b) => {
+          const dateA = new Date(a["date"]);
+          const dateB = new Date(b["date"]);
+          if (dateA < dateB) return -1;
+          if (dateA > dateB) return 1;
+          return a["activityOrder"] - b["activityOrder"];
+        });
+
+        return (
+          <div key={index}>
+            {sortedActivities.map((activity, index) => (
+              <div key={index}>
+                <ActivityCard activity={activity} />
+              </div>
+            ))}
+          </div>
+        );
+      })}
     </div>
   );
+}
+
+{
+  /* <ActivityCard activity={activity}></ActivityCard>; */
 }
