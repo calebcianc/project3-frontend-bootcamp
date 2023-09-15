1da0ae9fa7d0cacc04ddff6a065705e9852a9248
khloeli <chloelihuimin@gmail.com>
2023-09-16 00:08:22 +0800

    filter for itinerary which end date is more than or equal to today

--- src/Components/ExplorePage.js
+++ src/Components/ExplorePage.js
@@ -10,7 +10,7 @@ export default function ExplorePage({
   handleOnCardClick,
   setValue,
 }) {
-  console.log("itineraryActivities: ", { itineraryActivities });
+  // console.log("itineraryActivities: ", { itineraryActivities });
 
   if (itineraryActivities && itineraryActivities.length === 0) {
     return (
